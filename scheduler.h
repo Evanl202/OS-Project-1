961a2fc75f092c88ecf72fba4534fa67cf71b33f
Evanl202 <evanlin202@gmail.com>
2025-09-30 20:28:46 -0400

    Update scheduler.h
    Made structure for variables
    Made function calls

--- scheduler.h
+++ scheduler.h
@@ -1,1 +1,26 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
+#define MAX_PROCESSES 100
+
+//Structure of process
+typedef struct {
+  int pid;
+  int arrival_time;
+  int burst_time;
+  int priority;
+  int waiting_time;
+  int turnaround_time;
+  int completion_time;
+} Process;
+
+//Functions
+int readProcesses(const char *filename, Process processes[]);
+void printGantt(Process processes[], int n);
+void printResults(int gantt[], int time_line[], int count[]);
+
+//Algorithms
+void RoundRobin(Process processes[], int n, int quantum);
+void fcfs(Process processes[], int n);
+
+#endif
