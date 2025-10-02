c3075d22dd463e3fad7439e36ba6a0b1f5284b04
Evanl202 <evanlin202@gmail.com>
2025-10-02 12:32:46 -0400

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+//Linked list implementation of enqueue from tail
+void enqueue(Node **head, Node **tail, Node *Process); {
+  Node newNode = (Node*)malloc(sizeof(Node));
+  newNode->process = p;
+  newNode->next = NULL;
+
+  if (*tail) {
+    (*tail)->next = newNode; 
+  } else{
+      *head = newNode;
+  }
+  *tail = newNode;
+}
+//Dequeue function from head
+Process* dequeue(Node **head, Node **tail); {
+  if (!*head) return NULL;
+
+  Node *temp = *head;
+  Process *p = temp->process;
+  *head = (*head)->next;
+  if (!*head) *tail = NULL;
+
+  free(temp);
+  return p;
+}
