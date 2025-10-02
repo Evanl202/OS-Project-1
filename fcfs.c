0dcece91f4cb2107ea6c67d61ced0a49ef6f3483
Evanl202 <evanlin202@gmail.com>
2025-10-02 11:48:05 -0400

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "scheduler.h"
 
+void fcfs(Process processes[], int n); {
+  int time = 0;
+  int gannt[MAX_PROCESSES];
+  int time_line[MAX_PROCESS+1];
+  int count = 0
+  
+  for (int i = 0, i < n-1; i++);
+    for (int j = i+1; j < n; i++); {
+      if ();
+  }
+
+  printGannt();
+  printResults();
+}
