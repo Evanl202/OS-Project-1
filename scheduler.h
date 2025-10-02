b6929a0aa97fdf8562d3133a0d457b9d2a40eac2
Evanl202 <evanlin202@gmail.com>
2025-10-02 12:30:21 -0400

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,10 +1,7 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-//Functions
-int readProcesses(const char *filename, Process processes[]);
-void printGantt(Process processes[], int n);
-void printResults(int gantt[], int time_line[], int count[]);
+#include "process.h"
 
 //Algorithms
 void RoundRobin(Process processes[], int n, int quantum);
