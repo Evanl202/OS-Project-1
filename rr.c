2dd9e1e6c51a34a62312a329a0f5ff531d12dfb3
Evanl202 <evanlin202@gmail.com>
2025-10-02 21:47:27 -0400

    Update rr.c

--- rr.c
+++ rr.c
@@ -2,3 +2,8 @@
 #include "scheduler.h"
 #include "queue.h"
 #include "utils.c"
+
+void roundRobin(Process porcesses[], int n, int quantum) {
+  Node *head = NULL, *tail = NULL;
+  int time = 0, completed = 0;
+  int gantt
