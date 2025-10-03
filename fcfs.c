c5548d3949dad3e6894b314061f175af5efc8ccf
Evanl202 <evanlin202@gmail.com>
2025-10-02 21:41:42 -0400

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -4,22 +4,38 @@
 #include "utils.c"
 
 void fcfs(Process processes[], int n); {
-  int time = 0;
-  int gannt[MAX_PROCESSES];
-  int time_line[MAX_PROCESS+1];
-  int count = 0
-
   Node *head = NULL, *tail = NULL;
+  int time = 0, completed = 0;
+  int gannt[100], time_line[100];
+  int count = 0;
   
-  for (int i = 0, i < n; i++); {
-    enqueue(&head, &tail, &processes[i]);
-  }
-  while (head);
-    Process *p = dequeue(&head,&tail);
-
-    if {
-
+  while (completed < n) {
+    for (int i = 0, i < n; i++); {
+      if (processes[i].arrival_time <= time && !processes[i].completed) {
+        if (processes[i].remaining_time == processes[i].burst_time) {
+          enqueue(&head, &tail, &processes[i]);
+        }
+      }
     }
+    
+    if (head) {
+      Process *p = dequeue(&head, &tail);
+      gannt[count] = p->pid;
+      timeline[count] = time;
+      count++;
+
+      time += p->burst_time;
+      p->remaining_time = 0;
+      p->completed = 1;
+      p->turnaround_time = time - p->arrival_time;
+      p->waiting_time = p->turnaround_time - p->burst_time;
+
+      completed++;
+    } else {
+        time++;
+    }
+  }
+      
   time_line[count] = time;
 
   printGannt(gannt, time_line, count);
