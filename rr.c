76c308a99f72cf8efbc50d04c028f46fe36d046d
Evanl202 <evanlin202@gmail.com>
2025-10-02 22:06:56 -0400

    Update rr.c

--- rr.c
+++ rr.c
@@ -6,4 +6,53 @@
 void roundRobin(Process porcesses[], int n, int quantum) {
   Node *head = NULL, *tail = NULL;
   int time = 0, completed = 0;
-  int gantt
+  int gantt[100], time_line[100];
+  int count = 0;
+  
+  while (completed < n) {
+    for (int i = 0, i < n; i++); {
+      if (processes[i].arrival_time <= time && !processes[i].completed) {
+        if (processes[i].remaining_time == processes[i].burst_time) {
+          enqueue(&head, &tail, &processes[i]);
+        }
+      }
+    }
+    //Subtract time using quantum time
+    if (head) { 
+      Process *p = dequeue(&head, &tail);
+      int exec_time = (p->remaining_time > quantum) ? quantum : p->remaining_time;
+      
+      gannt[count] = p->pid;
+      timeline[count] = time;
+      count++;
+
+      time += exec_time;
+      p->remaining_time -= exec_time;
+
+      for (int i = 0; i < n; i++) {
+        if(processes[i].arrival_time > time_line[count - 1) && processes.arrival_time <= time & !processes[i].completed) {
+          if (processes[i].remaining_time == processes[i].burst_time) {
+            enqueue(&head, &tail, &processes[i]);
+          }
+        }
+      }
+      
+      if (p->remaining_time > 0) {
+        enqueue(&head, &tail, p);
+      } else {
+        p-> completed = 1;
+        p->turnaround_time = time - p->arrival_time;
+        p->waiting_time = p->turnaround_time - p->burst_time;
+
+        completed++;
+      }
+    } else {
+        time++;
+    }
+  }
+
+  time_line[count] = time;
+
+  printGannt(gantt, time_line, count);
+  printResults(processes, n);
+}
