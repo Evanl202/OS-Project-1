382b4b0e81cdee4a95e7a4d8778f2b9c9c1386e0
Evanl202 <evanlin202@gmail.com>
2025-10-06 15:15:37 -0400

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "scheduler.h"
 #include "queue.h"
-#include "utils.c"
+#include "utils.h"
 
-void fcfs(Process processes[], int n); {
+void fcfs(Process processes[], int n) {
   Node *head = NULL, *tail = NULL;
   int time = 0, completed = 0;
   int gantt[100], time_line[100];
   int count = 0;
   
   while (completed < n) {
-    for (int i = 0, i < n; i++); {
+    for (int i = 0; i < n; i++) {
       if (processes[i].arrival_time <= time && !processes[i].completed) {
         if (processes[i].remaining_time == processes[i].burst_time) {
           enqueue(&head, &tail, &processes[i]);
@@ -20,8 +20,8 @@ void fcfs(Process processes[], int n); {
     
     if (head) {
       Process *p = dequeue(&head, &tail);
-      gannt[count] = p->pid;
-      timeline[count] = time;
+      gantt[count] = p->pid;
+      time_line[count] = time;
       count++;
 
       time += p->burst_time;
@@ -38,6 +38,6 @@ void fcfs(Process processes[], int n); {
       
   time_line[count] = time;
 
-  printGannt(gantt, time_line, count);
+  printGantt(gantt, time_line, count);
   printResults(processes, n);
 }
