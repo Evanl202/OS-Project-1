fc54dce9b2f888762add23ac1d2715c25832f024
Evanl202 <evanlin202@gmail.com>
2025-10-06 14:55:28 -0400

    Update utils.c

--- utils.c
+++ utils.c
@@ -5,18 +5,21 @@
 #define MAX_PROCESS 100
 //function to read the data from the processes file
 int readProcesses (Process processes[]) {
-  FILE *file = fopen("processes.txt, "r");
-
+  FILE *file = fopen("processes.txt", "r");
+  if (file == NULL) {
+    printf("Error: could not open processes.txt")
+      return 0;
+  }
   int n = 0;
 
-  while f(fscanf(file, "%d %d %d", 
+  while (fscanf(file, "%d %d %d", 
       &processes[n].pid, 
       &processes[n].arrival_time, 
       &processes[n].burst_time) == 3) { 
-    &processes[n].waiting_time = 0;
-    &processes[n].turnaround_time = 0;
-    &processes[n].completed = 0;
-    &processes[n].remaining_time = process[n].burst_time;
+    processes[n].waiting_time = 0;
+    processes[n].turnaround_time = 0;
+    processes[n].completed = 0;
+    processes[n].remaining_time = processes[n].burst_time;
     n++;
   }
   fclose(file);
@@ -24,16 +27,17 @@ int readProcesses (Process processes[]) {
 }
 
 void printGantt (int gantt[], int time_line[], int count) {
-  printf("\nGantt Chart:\n") //loops through pid and prints 
-  for(int i = 0; i < count; i++) {
+  printf("\nGantt Chart:\n");
+  //loops through pid and prints 
+  for(int i = 0; i <= count; i++) {
     printf("| P%d ", gantt[i]);
   }
-  printf("|\n")
+  printf("|\n");
 
   for(int i = 0; i < count; int++) { //loops through pid completion time and prints
     printf("%d  ", time_line[i]);
   }
-  printf("\n")
+  printf("\n");
 }
 
 //prints result 
@@ -47,8 +51,8 @@ void printResults (Process processes[], int n) {
         processes[i].burst_time,
         processes[i].waiting_time,
         processes[i].turnaround_time,
-      total_wt += process[i].waiting_time;
-      total_tat += process[i].turnaround_time;
+      total_wt += processes[i].waiting_time;
+      total_tat += processes[i].turnaround_time;
   }
   printf("\nAverage waiting time = %.2f\n", total_wt / n);
   printf("Average turnaround time = %.2f\n", total_tat / n);
