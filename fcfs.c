9e10de9d5ceefc0e4f66641072646fd964501052
Evanl202 <evanlin202@gmail.com>
2025-10-06 20:35:03 -0400

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -9,17 +9,18 @@ void fcfs(Process processes[], int n) {
   int gantt[100], time_line[100];
   int count = 0;
   
-  while (completed < n) {
-    for (int i = 0; i < n; i++) {
-      if (processes[i].arrival_time <= time && !processes[i].completed) {
-        if (processes[i].remaining_time == processes[i].burst_time) {
+  for (int i = 0; i < n; i++) {
+      if (processes[i].arrival_time == 0) {
           enqueue(&head, &tail, &processes[i]);
-        }
       }
     }
-    
+  while (completed < n) {
     if (head) {
       Process *p = dequeue(&head, &tail);
+
+      if (time < p->arrival_time)
+        time = p->arrival_time;
+      
       gantt[count] = p->pid;
       time_line[count] = time;
       count++;
@@ -31,8 +32,22 @@ void fcfs(Process processes[], int n) {
       p->waiting_time = p->turnaround_time - p->burst_time;
 
       completed++;
+      for (int i = 0; i < n; i++) {
+          if(processes[i].arrival_time <= time &&
+            !processes[i].completed &&
+            processes[i].remaining_time == processes[i].burst_time) {
+            enqueue(&head, &tail, &processes[i]);
+          }
+      }
     } else {
         time++;
+        for (int i = 0; i < n; i++) {
+          if(processes[i].arrival_time <= time &&
+            !processes[i].completed &&
+            processes[i].remaining_time == processes[i].burst_time) {
+            enqueue(&head, &tail, &processes[i]);
+          }
+        }
     }
   }
       
